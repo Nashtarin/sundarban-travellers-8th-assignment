5d43734c261ff211604fc77a556aaa96af802046
Nashtarin Nur <nashtarin.nur@gmail.com>
2021-09-26 02:08:46 +0600

    header done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="head">
+            <h1>Lets Explore The Magnificent Sundarban</h1>
+            <p style={{ color: 'darkblue' }}>Don't miss the chance to travel the largest Mangrove forest in the world,join with us.</p>
+            <h1>Total Bugdet:600000 BDT</h1>
+        </div>
+    );
+};
+
+export default Header;
